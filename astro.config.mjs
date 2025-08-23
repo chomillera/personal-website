c29c780c519f58d97c619fb9e24580227653520f
Faisal Sugangga <faisalsugangga@gmail.com>
2025-08-23 17:46:55 +0700

    cf integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server' // <-- TAMBAHKAN BARIS INI
+  // <-- TAMBAHKAN BARIS INI
+  output: 'server',
+
+  adapter: cloudflare()
 });
